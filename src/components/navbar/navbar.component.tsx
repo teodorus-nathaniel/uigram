cfa7a25efea226175d0a5a624610db9790b07482
TeodorusNathaniel <nathankurnia26@gmail.com>
2020-03-04 23:17:29 +0700

    added hover effect

--- src/components/navbar/navbar.component.tsx
+++ src/components/navbar/navbar.component.tsx
@@ -5,20 +5,20 @@ import { Link } from 'react-router-dom';
 
 import Logo from './../../assets/images/logo.png';
 
-export default function Navbar (){
-  return (
-    <div className='navbar'>
-      <div className='navbar__logo'>
-        <img src={Logo} alt='Logo' />
-      </div>
-      <ul>
-        <li className='inverted'>
-          <Link to='/post'>Share your UI!</Link>
-        </li>
-        <li>
-          <Link to='/profile'>Profile</Link>
-        </li>
-      </ul>
-    </div>
-  );
+export default function Navbar() {
+	return (
+		<div className="navbar">
+			<div className="navbar__logo">
+				<img src={Logo} alt="Logo" />
+			</div>
+			<ul>
+				<li className="inverted">
+					<Link to="/post">Share your UI!</Link>
+				</li>
+				<li>
+					<Link to="/profile">Profile</Link>
+				</li>
+			</ul>
+		</div>
+	);
 }

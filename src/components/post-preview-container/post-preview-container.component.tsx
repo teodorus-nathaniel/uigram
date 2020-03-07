5d69ecf00c369f1478a183c19edfb2e74f10c8ac
TeodorusNathaniel <nathankurnia26@gmail.com>
2020-03-07 18:09:17 +0700

    added post entry animation

--- src/components/post-preview-container/post-preview-container.component.tsx
+++ src/components/post-preview-container/post-preview-container.component.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import PostPreview from '../post-preview/post-preview.component';
 import './post-preview-container.styles.scss';
 
-export default function PostPreviewContainer() {
-	const dummyData = Array.from({ length: 10 });
-	return (
-		<div className="post-preview-container">
-			{dummyData.map(() => <PostPreview />)}
-		</div>
-	);
+export default function PostPreviewContainer (){
+  const dummyData = Array.from({ length: 50 });
+
+  return (
+    <div className='post-preview-container'>
+      {dummyData.map((_, idx) => <PostPreview key={idx} />)}
+    </div>
+  );
 }
--- src/components/post-preview/post-preview.component.tsx
+++ src/components/post-preview/post-preview.component.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Page from './../../assets/images/page.jpg';
+import Page from './../../assets/images/Capture.png';
+// import Page from './../../assets/ima ges/page.jpg';
 
 import './post-preview.styles.scss';
 import CommentIcon from '../icons/comment/comment.component';
@@ -7,34 +8,44 @@ import BookmarkAddIcon from '../icons/bookmark-add/bookmark-add.component';
 import LikeDislike from '../like-dislike/like-dislike.component';
 import { Link } from 'react-router-dom';
 
-export default function PostPreview() {
-	return (
-		<div className="post-preview">
-			<div className="post-preview__content-container">
-				<Link to="/detail">
-					<div className="img-hover-hitbox" />
-					<img src={Page} alt="page" />
-				</Link>
-				<div className="post-preview__content">
-					<div className="post-preview__info">
-						<span className="post-preview__title">Figma UI</span>
-						<span className="post-preview__detail">
-							by{' '}
-							<Link to="/author" className="post-preview__author">
-								Teodorus
-							</Link>
-						</span>
-					</div>
-					<LikeDislike />
-				</div>
-			</div>
-			<div className="post-preview__additional">
-				<div className="post-preview__comments">
-					<CommentIcon size={1.2} />
-					<span>12 comments</span>
-				</div>
-				<BookmarkAddIcon size={1.2} />
-			</div>
-		</div>
-	);
+import { useInView } from 'react-intersection-observer';
+import useAnimation from '../../effects/useAnimation.effect';
+
+export default function PostPreview (){
+  const [ ref, , entry ] = useInView({
+    triggerOnce: true,
+    threshold: 0.5
+  });
+
+  useAnimation(entry!, { opacity: 1 }, 0.5);
+
+  return (
+    <div className='post-preview not-visible' ref={ref}>
+      <div className='post-preview__content-container'>
+        <Link to='/detail' className='image-container'>
+          <div className='img-hover-hitbox' />
+          <img src={Page} alt='page' />
+        </Link>
+        <div className='post-preview__content'>
+          <div className='post-preview__info'>
+            <span className='post-preview__title'>Figma UI</span>
+            <span className='post-preview__detail'>
+              by{' '}
+              <Link to='/author' className='post-preview__author'>
+                Teodorus
+              </Link>
+            </span>
+          </div>
+          <LikeDislike />
+        </div>
+      </div>
+      <div className='post-preview__additional'>
+        <div className='post-preview__comments'>
+          <CommentIcon size={1.2} />
+          <span>12 comments</span>
+        </div>
+        <BookmarkAddIcon size={1.2} />
+      </div>
+    </div>
+  );
 }

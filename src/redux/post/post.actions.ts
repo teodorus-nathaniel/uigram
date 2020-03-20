891e93ceaa4e7d68be01198650fde5de929a8508
TeodorusNathaniel <nathankurnia26@gmail.com>
2020-03-20 14:02:43 +0700

    added like dislike change

--- src/redux/post/post.actions.ts
+++ src/redux/post/post.actions.ts
@@ -2,8 +2,8 @@ import { action, payload } from 'ts-action';
 import { Post } from '../../@types/post.interfaces';
 
 export interface IFetchPostsPayload {
-	type: 'explore' | 'feeds';
-	sort?: string;
+  type: 'explore' | 'feeds';
+  sort?: string;
 }
 
 export const FETCH_POSTS_FAILURE = 'FETCH_POST_FAILURE';
@@ -11,21 +11,31 @@ export const FETCH_POSTS = 'FETCH_POSTS';
 export const LOAD_FEEDS_POSTS = 'LOAD_FEEDS_POSTS';
 export const LOAD_EXPLORE_POSTS = 'LOAD_EXPLORE_POSTS';
 export const CHANGE_SAVED = 'CHANGE_SAVED';
+export const CHANGE_LIKES_OR_DISLIKES = 'CHANGE_LIKES_OR_DISLIKES';
 
 export const fetchPostFailure = action(FETCH_POSTS_FAILURE, payload<Error>());
 export const fetchPosts = action(FETCH_POSTS, payload<IFetchPostsPayload>());
 export const loadFeedsPosts = action(LOAD_FEEDS_POSTS, payload<Post[]>());
 export const loadExplorePosts = action(LOAD_EXPLORE_POSTS, payload<Post[]>());
 export const changeSaved = action(
-	CHANGE_SAVED,
-	payload<{ id: string; saved: boolean }>()
+  CHANGE_SAVED,
+  payload<{ id: string; saved: boolean }>()
+);
+export const changeLikesOrDislikes = action(
+  CHANGE_LIKES_OR_DISLIKES,
+  payload<{
+    id: string;
+    likesChange: 1 | -1 | undefined;
+    dislikesChange: 1 | -1 | undefined;
+  }>()
 );
 
 type PostActionType =
-	| ReturnType<typeof fetchPostFailure>
-	| ReturnType<typeof fetchPosts>
-	| ReturnType<typeof loadExplorePosts>
-	| ReturnType<typeof loadFeedsPosts>
-	| ReturnType<typeof changeSaved>;
+  | ReturnType<typeof fetchPostFailure>
+  | ReturnType<typeof fetchPosts>
+  | ReturnType<typeof loadExplorePosts>
+  | ReturnType<typeof loadFeedsPosts>
+  | ReturnType<typeof changeSaved>
+  | ReturnType<typeof changeLikesOrDislikes>;
 
 export default PostActionType;
--- src/redux/post/post.reducer.ts
+++ src/redux/post/post.reducer.ts
@@ -1,70 +1,84 @@
 import { Post } from '../../@types/post.interfaces';
-import { CHANGE_SAVED } from './post.actions';
+import { CHANGE_SAVED, CHANGE_LIKES_OR_DISLIKES } from './post.actions';
 import PostActionType, {
-	FETCH_POSTS,
-	FETCH_POSTS_FAILURE,
-	LOAD_FEEDS_POSTS,
-	LOAD_EXPLORE_POSTS
+  FETCH_POSTS,
+  FETCH_POSTS_FAILURE,
+  LOAD_FEEDS_POSTS,
+  LOAD_EXPLORE_POSTS
 } from './post.actions';
 
 interface IState {
-	explore: Post[];
-	feeds: Post[];
-	isFetching: boolean;
-	error: Error | null;
+  explore: Post[];
+  feeds: Post[];
+  isFetching: boolean;
+  error: Error | null;
 }
 
 const INITIAL_STATE = {
-	explore: [],
-	feeds: [],
-	isFetching: false,
-	error: null
+  explore: [],
+  feeds: [],
+  isFetching: false,
+  error: null
 };
 
-export default function postReducer(
-	state: IState = INITIAL_STATE,
-	action: PostActionType
-): IState {
-	switch (action.type) {
-		case FETCH_POSTS:
-			return {
-				...state,
-				isFetching: true
-			};
-		case FETCH_POSTS_FAILURE:
-			return {
-				...state,
-				isFetching: false,
-				error: action.payload
-			};
-		case LOAD_FEEDS_POSTS:
-			return {
-				...state,
-				feeds: action.payload,
-				isFetching: false,
-				error: null
-			};
-		case LOAD_EXPLORE_POSTS:
-			return {
-				...state,
-				explore: action.payload,
-				isFetching: false,
-				error: null
-			};
-		case CHANGE_SAVED:
-			const exploreItem = state.explore.find(
-				(el) => el.id === action.payload.id
-			);
-			const feedsItem = state.feeds.find((el) => el.id === action.payload.id);
-			if (exploreItem) exploreItem.saved = action.payload.saved;
-			if (feedsItem) feedsItem.saved = action.payload.saved;
+export default function postReducer (
+  state: IState = INITIAL_STATE,
+  action: PostActionType
+): IState{
+  function updateExploreAndFeeds (
+    id: string,
+    operationCb: (item: Post) => void
+  ){
+    const exploreItem = state.explore.find((el) => el.id === id);
+    const feedsItem = state.feeds.find((el) => el.id === id);
+    if (exploreItem) operationCb(exploreItem);
+    if (feedsItem) operationCb(feedsItem);
 
-			return {
-				...state,
-				explore: state.explore.map((el) => ({ ...el })),
-				feeds: state.feeds.map((el) => ({ ...el }))
-			};
-		default:
-			return state;
-	}
+    return {
+      ...state,
+      explore: state.explore.map((el) => ({ ...el })),
+      feeds: state.feeds.map((el) => ({ ...el }))
+    };
+  }
+
+  switch (action.type) {
+    case FETCH_POSTS:
+      return {
+        ...state,
+        isFetching: true
+      };
+    case FETCH_POSTS_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload
+      };
+    case LOAD_FEEDS_POSTS:
+      return {
+        ...state,
+        feeds: action.payload,
+        isFetching: false,
+        error: null
+      };
+    case LOAD_EXPLORE_POSTS:
+      return {
+        ...state,
+        explore: action.payload,
+        isFetching: false,
+        error: null
+      };
+    case CHANGE_SAVED:
+      return updateExploreAndFeeds(
+        action.payload.id,
+        (item) => (item.saved = action.payload.saved)
+      );
+    case CHANGE_LIKES_OR_DISLIKES:
+      return updateExploreAndFeeds(action.payload.id, (item) => {
+        const { likesChange, dislikesChange } = action.payload;
+        if (likesChange) item.likeCount += likesChange;
+        if (dislikesChange) item.dislikeCount += dislikesChange;
+      });
+    default:
+      return state;
+  }
 }
